870f360600343fa13ac9b8c81c921375bc47d0e2
Hanako-O <hanakoodahara@gmail.com>
2020-11-24 14:29:58 +0900

    [訂正版]吹き出しの課題です

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,22 +4,27 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love Hanshin Tigers",//吹き出しのテキスト
-          255, 212, 0,//吹き出しの色
-          0, 0, 120, 30);//吹き出しの位置
-  //crossmark(20, 30, 30, 45)//吹き出しのしっぽ
+  t = "I love Hanshin Tigers"
+  a = 255
+  b = 212
+  c = 0
+  d = 50
+  e = 50
+  balloon(t, a, b, c, d, e);
   fill(255, 212, 0);
-  triangle(20, 30, 30, 40, 40, 30);
+  noStroke();
+  triangle(d + 10, e, d + 20, e, d + 15, e + 15);
 }
 
-function balloon(t, a, b, c, d, e, f, g){
+function balloon(t, a, b, c, d, e){
   let w = textWidth(t);
-  let h = textAscent() + textDescent();//文字の高さ
+  let h = textAscent(t) + textDescent(t);//文字の高さ
   let p = 2;//余白の大きさ
   fill(a, b, c);
-  rect(d, e, f, g);
-  fill(255);
-  text(t, p, h + p);
+  noStroke();
+  rect(d, e - 15, w, h + p * 2);
+  fill(0);
+  text(t, d, e);
 }
 
 //function crossmark(x1, y1, x2, y2){
